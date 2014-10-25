0d28efee7c8504e734d6a28b63034a4d8c3bf7da
Markus Török <markus.toeroek@gmail.com>
2014-10-25 16:19:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeSchedule::Application.config.secret_key_base = '9e7b70b16e5c67075c2c355226d61c69c336befc855e931e9c75cf5286ee82410304b63401319d77a8aea51d2d642070338f2203e3d6e37158a45248a5e4c724'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PracticeSchedule::Application.config.session_store :cookie_store, key: '_practice_schedule_session'
