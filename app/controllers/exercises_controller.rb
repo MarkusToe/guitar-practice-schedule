f2a6aafea838fdc9456a4678a00b8894e673e54e
Markus Török <markus.toeroek@gmail.com>
2014-10-25 19:53:01 +0200

    change key on done

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,8 +2,11 @@ class ExercisesController < ApplicationController
   def update
     @exercise = Exercise.find(params[:id])
 
-    if @exercise.update_attribute(:last_practiced, Time.now)
-      flash[:success] = "Exercise #{@exercise.name} done!"
+    current_key = @exercise.key
+    new_key = Key.where.not(id: current_key.id).order("RANDOM()").first
+
+    if @exercise.update_attribute(:last_practiced, Time.now) && @exercise.update_attribute(:key, new_key)
+      flash[:success] = "Exercise #{@exercise.name} in key #{new_key.name} done!"
       redirect_to root_path
     else
       flash[:error] = "Exercise could not be saved!"
